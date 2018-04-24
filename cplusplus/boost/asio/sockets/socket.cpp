f18aa0e27e0d55791ad74ffacd2fddb6c372c17a
xs233 <xs233@github.com>
2018-04-24 04:06:41 -0700

    add asio socket example

---
+++ cplusplus/boost/asio/sockets/socket.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <array>
+#include <boost/asio.hpp>
+
+void test_socket() {
+    boost::asio::io_service io_service;
+    boost::asio::ip::tcp::socket tcp_socket(io_service);
+    boost::asio::ip::tcp::resolver resolver(io_service);
+    boost::asio::ip::tcp::resolver::results_type endpoints = resolver.resolve("8.8.8.8", "daytime");
+    try {
+        boost::asio::connect(tcp_socket, endpoints);
+        while (true) {
+            std::array<char, 1024> buf;
+            boost::system::error_code error;
+            size_t len = tcp_socket.read_some(boost::asio::buffer(buf), error);
+            if (boost::asio::error::eof == error) {
+                std::cout << "eof" <<  '\n';
+            } else if (error) {
+                throw boost::system::system_error(error);
+            }
+
+            std::cout.write(buf.data(), len);
+        }
+    } catch (std::exception& e) {
+        std::cout << e.what() << '\n';
+    }
+    
+}
+
+int main() {
+    test_socket();
+    return 0;
+}
